97dcc91e5e0aa04dfd21cbfe45ac42e19e27daf7
Matt Lubel <matt_lubel@hotmail.com>
2016-09-11 20:54:23 +0100

    trouble shooting

--- script.js
+++ script.js
@@ -17,71 +17,71 @@ $(document).ready(function() {
 			searchLocs[i] = spherical.computeOffset(myLoc, RADIUS, i * 360 / NUMBER);
 			console.log(i, "th search loc computed");
 		}
+	}
+	
+	var geocoder = new google.maps.Geocoder();
 
-		function geocodeAddress(geocoder, resultsMap) {
-			var address = document.getElementById('address').value;
-			geocoder.geocode({
-				'address': address
-			}, function(results, status) {
-				if (status === 'OK') {
-					resultsMap.setCenter(results[0].geometry.location);
-					var marker = new google.maps.Marker({
-						map: resultsMap,
-						position: results[0].geometry.location
-					});
-				} else {
-					alert('Geocode was not successful for the following reason: ' + status);
-				}
-			});
-		};
-
-		var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-		var geocoder = new google.maps.Geocoder();
-
-		document.getElementById('submit').addEventListener('click', function() {
-			geocodeAddress(geocoder, map);
+	function geocodeAddress(geocoder, resultsMap) {
+		var address = document.getElementById('address').value;
+		geocoder.geocode({
+			'address': address
+		}, function(results, status) {
+			if (status === 'OK') {
+				resultsMap.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: resultsMap,
+					position: results[0].geometry.location
+				});
+			} else {
+				alert('Geocode was not successful for the following reason: ' + status);
+			}
 		});
+	};
 
-		infowindow = new google.maps.InfoWindow();
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-		var service = new google.maps.places.PlacesService(map);
+	document.getElementById('submit').addEventListener('click', function() {
+		geocodeAddress(geocoder, map);
+	});
 
-		for (var i = 0; i < NUMBER; i++) {
-			service.nearbySearch({
-				location: searchLocs[i],
-				radius: 2000,
-				//type: ['store']
-				type: ['post_office']
-			}, searchCallback);
-			console.log(i, "th search conducted.");
-		}
+	infowindow = new google.maps.InfoWindow();
 
-		var homemarker = new google.maps.Marker({
-			position: myLoc,
-			title: "Hello World!"
-		});
+	var service = new google.maps.places.PlacesService(map);
 
-		function searchCallback(results, status) {
-			if (status === google.maps.places.PlacesServiceStatus.OK) {
-				for (var i = 0; i < results.length; i++) {
-					createMarker(results[i]);
-				}
+	for (var i = 0; i < NUMBER; i++) {
+		service.nearbySearch({
+			location: searchLocs[i],
+			radius: 2000,
+			//type: ['store']
+			type: ['post_office']
+		}, searchCallback);
+		console.log(i, "th search conducted.");
+	}
+
+	var homemarker = new google.maps.Marker({
+		position: myLoc,
+		title: "Hello World!"
+	});
+
+	function searchCallback(results, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			for (var i = 0; i < results.length; i++) {
+				createMarker(results[i]);
 			}
 		}
+	}
 
-		function createMarker(place) {
-			var placeLoc = place.geometry.location;
-			var marker = new google.maps.Marker({
-				map: map,
-				position: place.geometry.location
-			});
+	function createMarker(place) {
+		var placeLoc = place.geometry.location;
+		var marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location
+		});
 
-			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.setContent(place.name);
-				infowindow.open(map, this);
-			});
-		}
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.setContent(place.name);
+			infowindow.open(map, this);
+		});
 	}
 
 });
